e86ae597d724d4470779444c4bc15a2110d88355
Marilyn <marycuorevilla50@gmail.com>
2019-06-17 10:04:11 +0200

    my first commit

---
+++ .history/js/main_20190616224920.js
@@ -0,0 +1,14 @@
+const nav = document.querySelector('.main-nav');
+const nav_height = nav.getBoundingClientRect().height
+window.addEventListener('scroll', e => {
+    let current_scroll = window.pageYOffset
+    console.log(current_scroll, nav_height)
+    if(current_scroll > nav_height) nav.classList.add('float')
+    else if(current_scroll < nav_height) nav.classList.remove('float')
+})
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems);
+  });
